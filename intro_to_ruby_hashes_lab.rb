97957279d3ccd6b1398d4dd9e8ba2d7ed3f2a386
Victor Jian Choe <vchoe89@gmail.com>
2019-07-25 22:44:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,21 +1,6 @@
 def monopoly
 	monopoly = {:railroad => {},
-	names => {
-	  :reading_railroad =>
-	  {
-	    "mortgage_value" => "$100"},
-	  :pennsylvania_railroad =>
-	  {
-	    "mortgage_value" => "$200"},
-	  :b_and_o_railroad =>
-	  {
-	    "mortgage_value" => "$400"},
-	  :shortline =>
-	  {
-	    "mortgage_value" => "$800"}
-      }
-    }
-  }
+	
 end
 
 
