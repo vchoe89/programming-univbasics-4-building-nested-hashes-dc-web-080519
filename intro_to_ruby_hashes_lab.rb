b6644f2da926a4e2a4b76b7f3ff10f83c4e9f871
Victor Jian Choe <vchoe89@gmail.com>
2019-07-25 22:44:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,14 +1,5 @@
 def monopoly
-	monopoly = {:railroad =>
-	{
-	  :pieces => 4,
-	  :rent_in_dollars =>
-	  {
-	    :one_piece_owned => 25,
-	    :two_pieces_owned => 50,
-	    :three_pieces_owned => 100,
-	    :four_pieces_owned => 200
-	  },
+	monopoly = {:railroad => {},
 	names => {
 	  :reading_railroad =>
 	  {
